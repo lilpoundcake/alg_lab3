608f36b600f80180e9285d7f820d0dd9e5e9d929
lilpoundcake <qwerty081295@gmail.com>
2019-04-11 21:45:55 +0700

    Добавил код для бинарного поиска.

--- search.py
+++ search.py
@@ -12,6 +12,19 @@ def linear_search(data, value):
 
 
 def binary_search(data, value):
-    # Реализуйте алгоритм здесь
-    pass
+    a = 0
+    b = len(data) - 1
+    c = round(b/2)
+    if data[c] == value:
+        return c
+    for i in range(1, len(data)):
+        if data[c] == value:
+            return c
+        elif value > data[c]:
+            a = c
+            c = round((a+b)/2)
+        elif value < data[c]:
+            b = c
+            c = round((a+b)/2)
+
 
